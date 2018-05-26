dab7d2cdce829e99cd0943dc54207bf5284e2ee7
adit <kulik2@ymail.com>
2018-05-26 13:14:58 -0700

    update sudo command

--- tectonic.sh
+++ tectonic.sh
@@ -2,11 +2,11 @@ wget https://github.com/coreos/matchbox/releases/download/v0.7.0/matchbox-v0.7.0
 gpg --verify matchbox-v0.7.0-linux-amd64.tar.gz.asc matchbox-v0.7.0-linux-amd64.tar.gz
 tar xzvf matchbox-v0.7.0-linux-amd64.tar.gz
 cd matchbox-v0.7.0-linux-amd64
-cp contrib/systemd/matchbox-for-tectonic.service /etc/systemd/system/matchbox.service
+sudo cp contrib/systemd/matchbox-for-tectonic.service /etc/systemd/system/matchbox.service
 cd scripts/tls
 export SAN=DNS.1:tectonic-installer,IP.1:192.168.254.131
 ./cert-gen
-mkdir /etc/matchbox
-cp ca.crt server.crt server.key /etc/matchbox
-systemctl daemon-reload
-systemctl start matchbox
+sudo mkdir /etc/matchbox
+sudo cp ca.crt server.crt server.key /etc/matchbox
+sudo systemctl daemon-reload
+sudo systemctl start matchbox
