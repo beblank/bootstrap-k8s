e941c6ad2bc430851fd983854fe491a129bd7070
Aditya Hadiwijaya <hadiwijaya.aditya@gmail.com>
2018-06-07 20:04:36 +0700

    uncomment remove and get coreos

--- provisioner.sh
+++ provisioner.sh
@@ -26,11 +26,11 @@ cd ~/matchbox/scripts/tls/
 . ~/matchbox/scripts/tls/cert-gen
 #Move TLS credentials to the matchbox server's default location.
 sudo mkdir -p /etc/matchbox
-sudo rm /etc/matchbox/ca.crt /etc/matchbox/server.crt /etc/matchbox/server.key
+#sudo rm /etc/matchbox/ca.crt /etc/matchbox/server.crt /etc/matchbox/server.key
 sudo cp ~/matchbox/scripts/tls/ca.crt ~/matchbox/scripts/tls/server.crt ~/matchbox/scripts/tls/server.key /etc/matchbox
 #Save client.crt, client.key, and ca.crt for later use (e.g.~/.matchbox).
 mkdir -p ~/.matchbox
-sudo rm ~/.matchbox/client.crt ~/.matchbox/client.key ~/.matchbox/ca.crt
+#sudo rm ~/.matchbox/client.crt ~/.matchbox/client.key ~/.matchbox/ca.crt
 sudo cp ~/matchbox/scripts/tls/client.crt ~/matchbox/scripts/tls/client.key ~/matchbox/scripts/tls/ca.crt ~/.matchbox/
 
-sudo ~/matchbox/scripts/get-coreos stable 1688.5.3 /var/lib/matchbox/assets
+#sudo ~/matchbox/scripts/get-coreos stable 1688.5.3 /var/lib/matchbox/assets
