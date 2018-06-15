40cc1ffecc56dc167ac1524efa0ec24e51878c43
adit <kulik2@ymail.com>
2018-06-15 01:48:36 -0700

    add some ignition files

--- ignition.sh
+++ ignition.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #-ex
-curl --retry 10 "http://192.168.254.144:8080/ignition?uuid=a82f4d56-498b-2b7b-7148-c1faca122ad4&mac=52-54-00-a1-9c-ae&os=installed" -o ignition.json
+read MAC
+DASHMAC = ${MAC//:/-}
+curl --retry 10 "http://192.168.254.144:8080/ignition?mac=$DASHMAC&os=installed" -o ignition.json
 coreos-install \
   -d /dev/sda \
   -C stable \
